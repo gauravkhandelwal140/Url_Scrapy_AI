5781e72f07bafd50c226d254413466416afcadff
gourav <gourav.khandelwal@bombaysoftwares.com>
2025-05-09 00:30:19 +0530

    add Index home page for UI

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from ai_v1 import extract_info, extract_info_with_question
 from config import SECRET_API_KEY
 from model import WebsiteInfo, WebsiteRequest, QARequest, QAResponse
 from scraper import scrape_homepage
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def Index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/scrape_data", response_model=WebsiteInfo)
-def analyze_website(data: WebsiteRequest,authorization: str = Header(None)):
+def analyze_website(data:WebsiteRequest,authorization: str = Header(None)):
     if authorization != f"Bearer {SECRET_API_KEY}":
         raise HTTPException(status_code=401, detail="Unauthorized")
 
